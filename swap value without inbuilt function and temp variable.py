c99cd995df9871ec49b52c50be11eea7aea5f983
ElangoManikandan <108584490+ElangoManikandan@users.noreply.github.com>
2023-12-13 13:05:09 +0530

    Add files via upload

---
+++ swap value without inbuilt function and temp variable.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the first value to be swapped(a):\n"))
+b=int(input("Enter the second value to be swapped(b):\n"))
+a=a+b #a=10+20...a=30
+b=a-b #b=30-20...b=10
+a=a-b #a=30-10...a=20
+print("swapped values(a,b):",a,b)
