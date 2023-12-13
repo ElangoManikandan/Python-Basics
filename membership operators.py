77057622665e6e9e3c647f529d69201c23abb5ff
ElangoManikandan <108584490+ElangoManikandan@users.noreply.github.com>
2023-12-13 15:12:53 +0530

    Add files via upload

---
+++ membership operators.py
@@ -0,0 +1,20 @@
+a=("12","24","36")
+b=("24","36","64")
+print( "24" in a)
+print("24" not in a)
+
+a=["12","24","36"]
+b=["24","36","64"]
+print( "36" in a)
+print("36" not in a)
+
+a={"12","24","36"}
+b={"24","36","64"}
+print( "36" in a)
+print("36" not in a)
+
+a1={1:"vikram",2:"jigardhanda",3:"Vedhalam"}
+b1={4:"Ennai arindhal:",5:"Theri",6:"Iraivi"}
+print( 1 in a1)#use key for accessing membership operator in dictionary 
+print(6 in b1)
+
